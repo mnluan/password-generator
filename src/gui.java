4fc271bfd43a961c9df573be15f11d2bd763def4
mnluan <luanlm@live.com>
2021-09-24 22:53:09 -0300

    Added more security

--- src/gui.java
+++ src/gui.java
@@ -25,7 +25,7 @@ public class gui{
 	//code elements
 	String number = "0,1,2,3,4,5,6,7,8,9";
 	String lower = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
-	String special = "!,?,.,_,-,+,;,:,";
+	String special = "!,?,.,_,-,+,=,;,:,{,},(,),[,],$";
 	String aux = "";
 	
 	gui(){
